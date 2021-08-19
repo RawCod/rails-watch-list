d38d49aed70fa8816d7581ae0ef8675492c1afae
Susheel Shivaprasad <susheelshivaprasad@gmail.com>
2021-08-19 16:16:11 +0100

    controllers created and relevant view page files made without form at the moment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list routes
+  # see all lists lists
+  # details of given list lists/42
+  # create new list get lists/new post lists
+  resources :lists, only: %i[index show new create] do
+
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Movie.destroy_all
+puts "Database cleaned"
+
+puts "Creating movies"
+ 50.times do
+   movie = Movie.create(title: Faker::Movie.title, overview: Faker::GreekPhilosophers.quote)
+   puts "Movie #{movie.name} has been created!"
+ end
+
+ puts "#{Movie.count} have been created! "
